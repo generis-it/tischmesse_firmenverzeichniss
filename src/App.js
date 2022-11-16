cc7498020ab45b39f25a25505cd20bab3e9f65a1
Elias Bräm <elias.braem@generis.ch>
2022-11-16 16:59:47 +0100

    filter

--- src/App.js
+++ src/App.js
@@ -9,76 +9,62 @@ var Airtable = require('airtable');
 var base = new Airtable({apiKey: 'keyigRrBXDdtbuUQ3'}).base('appShZ2e3RAuNGWGt');
 
 const [aussteller, setAussteller] = useState([]);
+const [filterString, setfilterString] = useState("");
 const [ins, setIns] = useState([]);
-
+const [content, setContent] = useState([]);
 
 //Ap anfragen und records in Hook speichern
+
+const suchstring = 'SEARCH("'+filterString.toLowerCase() +'", LOWER({Firmenname}))'
+//const suchstringLowecase = filter
+
 useEffect(() => {
 
-  base('Aussteller').select({view: "Grid view"})
+  base('Aussteller').select({filterByFormula: suchstring, view: "Grid view"})
   .eachPage((records, fetchNextPage) => {
   setAussteller(records);
-  console.log(records);
   fetchNextPage();
   });
   base('Inserate').select({view: "Grid view"})
   .eachPage((records, fetchNextPage) => {
   setIns(records);
-  console.log(records);
+
   fetchNextPage();
-
-  //shuffle elements mit werbung
-  
-  var ul = document.querySelector('#aussteller-cards');
-  for (var i = ul.children.length; i >= 0; i--) {
-    ul.appendChild(ul.children[Math.random() * i | 0]);}
-
   });
-}, []);
+  
+}, [filterString]);
 
-// let opencards = [];
-
-// const changeCard = (id) => {
-//     const cardMiddle = document.getElementById("card_detail_1");
-//     const button = document.getElementById("expand_Button_1");
-//     if (opencards.includes(id)) {
-//         console.log("gefunden");
-//         opencards.pop(id);
-//         cardMiddle.style.display = 'none';
-//         button.src = "plus.svg";
-
-//     } else {
-//         console.log("nicht gefunden");
-//         cardMiddle.style.display = 'flex';
-//         opencards.push(id);
-//         button.src = "minus.svg";
-//     }
-// }
-
+const setFilter = (filter) => {
+  console.log(filterString);
+  setfilterString(filter.target.value);
+}
 
   return (
     <div className="App">
       <header>
         <h1>Tischmesse 2023</h1>
-        <h2>Ausstellerverzeichnis</h2>
-        {/* <input type="text" placeholder="Suche..."/> */}
+        <h2>Firmenverzeichnis</h2>
+         <input type="text" placeholder="Suche..." onChange={setFilter}/>
     </header>
     <main>
       <div id="aussteller-cards">
+      
+{   
+
+      aussteller.map((e)=> (
+        <Aussteller key={e.id} aussteller={e.fields}/>
+      ))
+      }
       {
       ins.map((e)=> (
         <Inserat screen="mobile" bild={e.fields.Inserat[0].url}/>
       ))}
-{
-      aussteller.map((e)=> (
-        <Aussteller key={e.id} aussteller={e.fields} />
-      ))}
-      
       </div>
       <div id="sidebar">
-      {ins.map((e)=> (
-        <Inserat screen="desktop" bild={e.fields.Inserat[0].url}/>
+      {ins.map((e, index)=> (
+        <Inserat screen="desktop" key={e.id} bild={e.fields.Inserat[0].url}/>
       ))}
+      
       </div>
   </main>
     </div>
--- src/Aussteller.jsx
+++ src/Aussteller.jsx
@@ -2,53 +2,105 @@
 import React from 'react';
 import plus from './img/plus.svg';
 import minus from './img/minus.svg';
+import angebot from './img/angebot.svg';
+import map from './img/map.svg';
+import person from './img/person.svg';
+import person2 from './img/person2.svg';
+import telefon from './img/telefon.svg';
+import internet from './img/internet.svg';
 import { useState, useEffect } from 'react';
 
-const Aussteller = ({ id, aussteller}) => {
+const Aussteller = ({ id, aussteller }) => {
     const [open, setOpen] = useState(false);
+    const [image, setImage] = useState(false);
+    const [pers, setPers] = useState(false);
 
+    useEffect(() => {
+
+        try {
+            const url = aussteller.Logo[0].url;
+            setImage(true);
+            const person = aussteller.Person2;
+            setPers(person.length>=2);
+
+        } catch (error) {
+            console.log("bild von " + { aussteller } + "nicht gefunden")
+        }
+
+    }, []);
 
 
     let styles = {
         borderColor: aussteller.Farbe,
-      };
-    
+    };
+
     return (
-    <div className="article"  style={styles}>
-        <div className="card_top" >
-            <h3>{aussteller.Firmenname}</h3>
-        {!open &&   <img src={plus} alt="plus" onClick={() => setOpen(!open)}/>} 
-        {open &&   <img src={minus} alt="plus" onClick={() => setOpen(!open)}/>} 
-      
-        </div>
-       <div className='card_content'>
+        <div className="article" style={styles}>
+            <div className=' card_content_top'>
 
-        <div className='card_middle'>
-        <img src={aussteller.Logo[0].url} alt="" />
-            <div className="label_box">
-                    <div className="tag">label</div>
-                    <div className="tag">label</div>
-            </div>
-        </div>
-        {open &&   <div id={id} className="card_detail_1">
-                <p>{aussteller.Beschreibung}
-                </p>
 
-            </div>}
-       
+                <div className="card_top" >
+                    <h3>{aussteller.Firmenname}</h3>
+                    <div className='card_top_right'>
+                        <h4>Tisch Nr. {aussteller.Tischnummer}</h4>
+                        {!open && <img src={plus} alt="plus" onClick={() => setOpen(!open)} />}
+                        {open && <img src={minus} alt="plus" onClick={() => setOpen(!open)} />}
+                    </div>
 
-            <div className="card_bottom">
-                <div className="icon_text">
-                    
-                    <p>info@internetter.ch</p>
                 </div>
-                <div className="icon_text">
-                   
-                    <p>078 826 31 08</p>
+                <div className='card_middle'>
+                    {image && <img src={aussteller.Logo[0].url} alt="" />}
+                    <div className="label_box">
+                        <div className="tag">{aussteller.branche}</div>
+                    </div>
                 </div>
             </div>
-       </div>
-    </div>)
+            <div className='card_content'>
+
+
+                {open && <>
+                    <div className="icon_text">
+                        <img src={angebot} alt="angebot" />
+                        <p>{aussteller.Angebot}</p>
+                    </div>
+                    <div className="icon_text">
+                        <img src={person} alt="person" />
+                        <p>{aussteller.Person1} </p>
+                    </div>
+                    {pers && <div className="icon_text" >
+                        <img src={person2} alt="person"  />
+                        <p  className='pers2'>{aussteller.Person2} </p>
+                    </div>}
+
+                    <div className="icon_text">
+                        <img src={map} alt="plus" />
+                        <p>{aussteller.ort}</p>
+                    </div>
+                    <div id={id} className="card_detail_1">
+                        <p>{aussteller.Beschreibung}
+                        </p>
+
+                    </div>
+                
+
+
+                </>
+                }
+
+
+
+                <div className="card_bottom">
+                <div className="icon_text">
+                        <img src={internet} alt="internet" />
+                        <a href={aussteller.url} target="_blank"><p>{aussteller.website}</p></a>
+                    </div>
+                    <div className="icon_text">
+                        <img src={telefon} alt="plus" />
+                        <p>{aussteller.telefon}</p>
+                    </div>
+                </div>
+            </div>
+        </div>)
 }
 export default Aussteller
 
